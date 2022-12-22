a9e878c99baa21ae5e4e6733bf45534535c230ef
aeong <aeong@aeongui-MacBookPro.local>
2022-12-22 23:38:33 +0900

    1222

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,15 +1,23 @@
 import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
-
+const API_KEY = "8a4d9b4faf273b1024f10e9c8d2086ec";
 export default function App() {
   const [city, setCity] = useState("Loading...");
-  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -18,17 +26,21 @@ export default function App() {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync({ accuracy: 5 });
     const location = await Location.reverseGeocodeAsync(
-      {
-        latitude,
-        longitude,
-      },
+      { latitude, longitude },
       { useGoogleMaps: false }
     );
-    console.log(location[0].city);
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alert&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
   };
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
+  let text = "Waiting..";
+
   return (
     <View style={styles.container}>
       {/* <StatusBar style="auto" /> */}
@@ -39,32 +51,32 @@ export default function App() {
         indicatorStyle="white"
         pagingEnabled
         horizontal
+        showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
+        {days?.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days?.length > 0 &&
+          days?.map((days, index) => (
+            <View key={index} style={styles.day}>
+              <Text>{text}</Text>
+              <Text style={styles.temp}>
+                {parseFloat(days.temp.day).toFixed(1)}
+              </Text>
+              <Text style={styles.desc}>{days.weather[0].main}</Text>
 
-        <View style={styles.day}>
-          <Text style={styles.temp}>-4</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
-
-        <View style={styles.day}>
-          <Text style={styles.temp}>-4</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
-
-        <View style={styles.day}>
-          <Text style={styles.temp}>-4</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
-
-        <View style={styles.day}>
-          <Text style={styles.temp}>-4</Text>
-          <Text style={styles.desc}>Sunny</Text>
-        </View>
+              <Text style={styles.tinyText}>{days.weather[0].description}</Text>
+              <Text>{Math.ceil(daily?.temp.max)}°</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -98,5 +110,8 @@ const styles = StyleSheet.create({
   desc: {
     marginTop: -30,
     fontSize: 60,
+  },
+  tinyText: {
+    fontSize: 20,
   },
 });
